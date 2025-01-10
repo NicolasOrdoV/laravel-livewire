6a7fb626d6d2f9801f48c9b2e1845f559d021265
NicolasOrdoV <65665096+NicolasOrdoV@users.noreply.github.com>
2025-01-09 21:08:40 -0500

    filtros

--- app/Livewire/Dashboard/Post/Index.php
+++ app/Livewire/Dashboard/Post/Index.php
@@ -2,32 +2,76 @@
 
 namespace App\Livewire\Dashboard\Post;
 
+use App\Models\Category;
 use App\Models\Post;
+use Livewire\Attributes\Url;
 use Livewire\Component;
 use Livewire\WithPagination;
 
 class Index extends Component
 {
+
+    #[Url]
+    public $type;
+    #[Url]
+    public $category_id;
+    #[Url]
+    public $posted;
+    public $search;
+    #[Url]
+    public $from;
+    #[Url]
+    public $to;
+
+
     use WithPagination;
     public $cofirmingDeletePost;
     public $postToDelete;
 
     public function render()
     {
-        $posts = Post::paginate(2);
-        return view('livewire.dashboard.post.index', compact('posts'));
+        // $posts = Post::where('id', '>=', '1');
+        $posts = new Post();
+        $categories = Category::get();
+
+        if ($this->type) {
+            $posts = $posts->where('type', $this->type);
+        }
+
+        if ($this->category_id) {
+            $posts = $posts->where('category_id', $this->category_id);
+        }
+
+        if ($this->posted) {
+            $posts = $posts->where('posted', $this->posted);
+        }
+
+        if ($this->search) {
+            $posts = $posts->where(function ($query) {
+                $query->orWhere('id', 'like', '%' . $this->search . '%')
+                    ->orWhere('title', 'like', '%' . $this->search . '%')
+                    ->orWhere('description', 'like', '%' . $this->search . '%');
+            });
+        }
+
+        if ($this->from && $this->to) {
+            $posts = $posts->whereBetween('date', [date($this->from), date($this->to)]);
+        }
+
+        $posts = $posts->paginate(20);
+        return view('livewire.dashboard.post.index', compact('posts', 'categories'));
     }
 
     public function delete(/*Category $category*/)
-        {
-            $this->postToDelete->delete();
-            $this->cofirmingDeletePost = false;
-            $this->dispatch('deleted');
-        }
+    {
+        $this->postToDelete->delete();
+        $this->cofirmingDeletePost = false;
+        $this->dispatch('deleted');
+    }
 
-        public function selectPostToDelete(Post $post)
-        {
-            $this->cofirmingDeletePost = true;
-            $this->postToDelete = $post;
-        }
+    public function selectPostToDelete(Post $post)
+    {
+        $this->cofirmingDeletePost = true;
+        $this->postToDelete = $post;
+    }
 }
--- resources/views/livewire/dashboard/post/index.blade.php
+++ resources/views/livewire/dashboard/post/index.blade.php
@@ -1,6 +1,4 @@
 <div>
-
-
     <div class="container">
         <x-action-message on="deleted">
             <div class="box-action-message">
@@ -18,12 +16,37 @@
                 Post
             @endslot
             <a class="btn-secondary mt-3" href="{{ route('d-post-create') }}">+ Create</a>
+            <br>
+            <div class="grid grid-cols-2 mb-3">
+                <select class="block w-full" wire:model.live='posted'>
+                    <option value=''>{{ __('Posted') }}</option>
+                    <option value='yes'>{{ __('Yes') }}</option>
+                    <option value='not'>{{ __('Not') }}</option>
+                </select>
+                <select class="block w-full" wire:model.live='type'>
+                    <option value=''>{{ __('Type') }}</option>
+                    <option value='advert'>{{ __('Advert') }}</option>
+                    <option value='post'>{{ __('Post') }}</option>
+                    <option value='course'>{{ __('Course') }}</option>
+                    <option value='page'>{{ __('Page') }}</option>
+                </select>
+                <select class="block w-full" wire:model.live='category_id'>
+                    <option value=''>{{ __('Category') }}</option>
+                    @foreach ($categories as $c)
+                        <option value='{{ $c->id }}'>{{ $c->title }}</option>
+                    @endforeach
+                </select>
+                <x-input wire:model.live='search' placeholder="{{__('Search...')}}"></x-input>
+                <div class="grid grid-cols-2 gap-2">
+                    <x-input class="w-full" type="date" wire:model='from' placeholder="Desde"></x-input>
+                    <x-input class="w-full" type="date" wire:model.live='to' placeholder="Hasta"></x-input>
+                </div>
+            </div>
             <table class="table w-full border">
                 <thead class="text-left bg-gray-50">
                     <tr class="border-b">
                         <th class="p-2">Id</th>
                         <th class="p-2">Title</th>
-                        <th class="p-2">Text</th>
                         <th class="p-2">Description</th>
                         <th class="p-2">Category</th>
                         <th class="p-2">Date</th>
@@ -34,9 +57,10 @@
                     @foreach ($posts as $post)
                         <tr class="border-b">
                             <td class="p-2">{{ $post->id }}</td>
-                            <td class="p-2">{{ $post->title }}</td>
-                            <td class="p-2">{{ $post->text }}</td>
-                            <td class="p-2">{{ $post->description }}</td>
+                            <td class="p-2">{{ str($post->title)->substr(0, 15) }}</td>
+                            <td class="p-2">
+                                <textarea> {{ $post->description }}</textarea>
+                            </td>
                             <td class="p-2">{{ $post->category->title }}</td>
                             <td class="p-2">{{ $post->date }}</td>
                             <td class="p-2">
@@ -49,7 +73,7 @@
                         </tr>
                     @endforeach
                 </tbody>
-                {{ $posts->links() }}
+
                 <x-confirmation-modal wire:model="cofirmingDeletePost">
                     @slot('title')
                         {{ __('Delete Post') }}
@@ -67,6 +91,7 @@
                     @endslot
                 </x-confirmation-modal>
             </table>
+            {{ $posts->links() }}
         </x-card>
     </div>
 </div>
